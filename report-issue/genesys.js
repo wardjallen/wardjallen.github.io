baa0ae28fb31c0a80f708c5cb75d3d147658c142
Jeff Ward <wardjallen@gmail.com>
2023-03-18 11:41:15 -0400

    ddf

--- report-issue/genesys.js
+++ report-issue/genesys.js
@@ -44,3 +44,58 @@ customPlugin.subscribe('WebChatService.started', function (e) {
 customPlugin.subscribe('WebChatService.ended', function (e) {
       console.log('Chat ended', e);
     });
+
+function getAdvancedConfig() {
+      return {
+            form: {
+                  autoSubmit: false,
+			firstname: 'Praenomen',
+			lastname: 'Gens',
+			email: 'praenomen.gens@calidumlitterae.com',
+			subject: 'Populated from data'
+            },
+            formJSON: {
+                  wrapper: '<table></table>',
+			inputs: [
+                        // Default fields
+				{
+                              id: 'cx_webchat_form_firstname',
+					name: 'firstname',
+					maxlength: '100',
+					placeholder: 'Required',
+					label: 'First Name'
+                        },
+                        {
+					id: 'cx_webchat_form_lastname',
+					name: 'lastname',
+					maxlength: '100',
+					placeholder: 'Required',
+					label: 'Last Name'
+				},
+				{
+					id: 'cx_webchat_form_email',
+					name: 'email',
+					maxlength: '100',
+					placeholder: 'Optional',
+					label: 'Email'
+				},
+				{
+				      id: 'cx_webchat_form_subject',
+					name: 'subject',
+					maxlength: '100',
+					placeholder: 'Optional',
+					label: 'Subject'
+				},
+				// Custom Fields
+				{
+					id: 'todoNumber',
+					name: 'todoNumber',
+					maxlength: '100',
+					placeholder: 'Todo Number',
+					label: 'Todo Number',
+					value: 'Enter number'
+				}
+                  ]
+            }
+      };
+}
