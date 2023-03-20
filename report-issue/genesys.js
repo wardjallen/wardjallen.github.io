7df2d3277c427f0414a6dcf819bcb12b31eb8b23
Jeff Ward <wardjallen@gmail.com>
2023-03-20 13:54:15 -0400

    ff

--- report-issue/genesys.js
+++ report-issue/genesys.js
@@ -2,14 +2,58 @@ const chatParams = new URLSearchParams(location.search);
 const device = chatParams.get('device');
 const roomName = chatParams.get('roomname');
 
+window._genesys = {
+    widgets: {
+      main: {
+        downloadGoogleFont: true,
+        googleFontUrl: 'https://apps.usw2.pure.cloud/webfonts/roboto.css'
+      },
+      webchat: {
+        transport: {
+          type: 'purecloud-v2-sockets',
+          dataURL: 'https://api.usw2.pure.cloud',
+          deploymentKey : 'da82457d-4a3e-45d2-a852-1cd7636f3e49',
+          orgGuid : '562da0e4-6065-4c91-9973-613e209b8e46',
+          interactionData: {
+            routing: {
+              targetType: 'QUEUE',
+              targetAddress: 'Jeffs Test Queue',
+              priority: 2
+            }
+          }
+        },
+        userData: {
+          addressStreet: '64472 Brown Street',
+          addressCity: 'Lindgrenmouth',
+          addressPostalCode: '50163-2735',
+          addressState: 'FL',
+          phoneNumber: '1-916-892-2045 x293',
+          phoneType: 'Cell',
+          customerId: '59606',
+        }
+      }
+    }
+  };
+  
+  
+  const customPlugin = CXBus.registerPlugin('Custom');
+  
+  customPlugin.subscribe('WebChatService.started', function (e) {
+    console.log('Chat started', e);
+  });
+  
+  customPlugin.subscribe('WebChatService.ended', function (e) {
+    console.log('Chat ended', e);
+  });
+
 
 function getAdvancedConfig() {
     return {
         form: {
             autoSubmit: false,
-            firstname: 'Praenomen',
-            lastname: 'Gens',
-            email: 'praenomen.gens@calidumlitterae.com',
+            firstname: '',
+            lastname: '',
+            email: '',
             subject: 'Issue with ' + device
         },
         formJSON: {
