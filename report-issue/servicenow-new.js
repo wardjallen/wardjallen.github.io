b547cb244bb864146e73b2c390d3009c4425c4ed
Jeff Ward <wardjallen@gmail.com>
2023-03-21 13:06:53 -0400

    ssdsd

--- report-issue/servicenow-new.js
+++ report-issue/servicenow-new.js
@@ -15,11 +15,11 @@ async function createReport(issueCategory, comment, person, room, device) {
 
   const title = issueCategory + ' / ' + room;
   const incident = {
-    assignment_group: 'MeetingRoom',
-    short_description: title,
-    urgency: '2',
-    impact: '2',
-    description: `User Comment: ${comment} \nUser Name: ${person} \nRoom: ${room} \nDevice: ${device}`,
+    "assignment_group": "MeetingRoom",
+    "short_description": title,
+    "urgency": "2",
+    "impact": "2",
+    "description": `User Comment: ${comment} \nUser Name: ${person} \nRoom: ${room} \nDevice: ${device}`,
   };
 
   const { instance, username, password } = servicenow;
@@ -27,15 +27,20 @@ async function createReport(issueCategory, comment, person, room, device) {
   // Send the incident payload to ServiceNow
   const url = `https://${instance}.service-now.com/api/now/table/incident`;
   const body = JSON.stringify(incident);
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': basicAuth(username, password),
-  }
+  const headers = new Headers();
+  headers.append("Authorization", "Basic YWRtaW46WGVAXjM5ZUVBN0w=");
+  headers.append("Content-Type", "application/json");
+  headers.append("Cookie", "BIGipServerpool_dev147948=864465930.53566.0000; JSESSIONID=AB98D1600B5B2F28332C29E1683BF371; glide_session_store=DCE7CAD897F92150538330C11153AFEB; glide_user_route=glide.47eb8ebe42faf2f1d4be0672c1a87ec1");
+  // const headers = {
+  //   'Content-Type': 'application/json',
+  //   'Authorization': basicAuth(username, password),
+  // }
 
   const options = {
-    headers,
+    headers: headers,
     method: 'POST',
-    body
+    body: body,
+    redirect: 'follow'
   };
 
   if (!instance || !username || !password) {
